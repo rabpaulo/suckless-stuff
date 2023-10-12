3ae11810ef1dd9df3b04c0f9a2ae9407a4899214
rabpaulo <zemitoblz@gmail.com>
2023-10-12 17:02:40 -0300

    update

--- st/config.h
+++ st/config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "monospace:pixelsize=13:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
-static int borderpx = 0;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
