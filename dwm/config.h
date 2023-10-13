4e5ad1cf5b7126145ed95461029b0235dc21e97b
rabpaulo <zemitoblz@gmail.com>
2023-10-12 22:30:34 -0300

    update

--- dwm/config.h
+++ dwm/config.h
@@ -18,12 +18,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "mono:size=9:antilias=true",  "NotoColorEmoji:pixelsize=12:autohint=true"};
 static const char dmenufont[]       = "mono:size=9";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#D2B48C";
-static const char light_brown[]        = "#D2B48C";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
